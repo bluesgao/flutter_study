af6a78f6a1ea748e86be37baedd28e8900c3e75b
blues <>
2025-02-15 16:45:51 +0800

    谷歌底部导航栏

--- navtab_demo/lib/pages/home.dart
+++ navtab_demo/lib/pages/home.dart
@@ -1,5 +1,6 @@
 import 'package:navtab_demo/pages/constants.dart';
-
+import 'package:google_nav_bar/google_nav_bar.dart';
+import 'package:line_icons/line_icons.dart';
 import 'index.dart';
 
 class HomePage extends StatefulWidget {
@@ -69,7 +70,8 @@ class HomePageState extends State<HomePage> {
       // appBar: AppBar(
       //   title: Text('岛上码农'),
       // ),
-      bottomNavigationBar: _buildBottomNavigationBar(),
+      // bottomNavigationBar: _buildBottomNavigationBar(),
+      bottomNavigationBar: _buildGoogleNavBar(),
       body: PageView(
         physics: NeverScrollableScrollPhysics(), //禁止手势滑动
         controller: _pageController,
@@ -79,7 +81,7 @@ class HomePageState extends State<HomePage> {
     );
   }
 
-  // 底部导航栏
+  // 原生底部导航栏
   BottomNavigationBar _buildBottomNavigationBar() {
     return BottomNavigationBar(
       type: BottomNavigationBarType.fixed,
@@ -121,6 +123,54 @@ class HomePageState extends State<HomePage> {
               height: 28,
             ),
       label: title,
+    );
+  }
+
+  // 谷歌底部导航栏
+  Container _buildGoogleNavBar() {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            blurRadius: 20,
+            color: Colors.black.withOpacity(.1),
+          )
+        ],
+      ),
+      child: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 8),
+          child: GNav(
+            rippleColor: const Color.fromARGB(255, 218, 109, 109),
+            hoverColor: const Color.fromARGB(255, 143, 84, 84),
+            tabActiveBorder: Border.all(color: Colors.grey.shade300),
+            gap: 8,
+            activeColor: Colors.black,
+            iconSize: 24,
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+            duration: Duration(milliseconds: 400),
+            tabBackgroundColor: Colors.grey[100]!,
+            color: Colors.black,
+            tabs: [
+              GButton(
+                icon: LineIcons.gamepad,
+                text: 'Games',
+              ),
+              GButton(
+                icon: LineIcons.history,
+                text: 'Likes',
+              ),
+              GButton(
+                icon: LineIcons.user,
+                text: 'Profile',
+              ),
+            ],
+            selectedIndex: _index,
+            onTabChange: _onBottomNagigationBarTapped,
+          ),
+        ),
+      ),
     );
   }
 }
