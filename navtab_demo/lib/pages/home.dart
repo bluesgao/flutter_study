3eab3e2ce29c68ab9cd4bb7657dd78fa601e1628
blues <>
2025-02-15 17:52:24 +0800

    定制原生底部导航栏

--- navtab_demo/lib/pages/home.dart
+++ navtab_demo/lib/pages/home.dart
@@ -34,6 +34,74 @@ class HomePageState extends State<HomePage> {
     });
   }
 
+  // 使用PageView实现
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // appBar: AppBar(
+      //   title: Text('岛上码农'),
+      // ),
+      // bottomNavigationBar: _buildBottomNavigationBar(),
+
+      bottomNavigationBar: _buildBottomNavigationBar(),
+      body: PageView(
+        physics: NeverScrollableScrollPhysics(), //禁止手势滑动
+        controller: _pageController,
+        onPageChanged: _onPageChanged,
+        children: _pageList,
+      ),
+    );
+  }
+
+  // 原生底部导航栏（去掉点击水波纹效果和点击高亮效果）
+  Theme _buildBottomNavigationBar() {
+    return Theme(
+        data: ThemeData(
+          splashColor: Colors.transparent, // 去掉点击水波纹效果
+          highlightColor: Colors.transparent, // 去掉点击高亮效果
+        ),
+        child: BottomNavigationBar(
+          backgroundColor: Colors.white, // 背景色
+          type: BottomNavigationBarType.fixed, // 类型
+          currentIndex: _index, // 当前索引
+          onTap: _onBottomNagigationBarTapped, // 点击事件
+          selectedItemColor: Colors.grey.shade900, //选中lable颜色
+          unselectedItemColor: Colors.grey.shade800, //未选中lable颜色
+          selectedLabelStyle:
+              TextStyle(fontSize: 12, fontWeight: FontWeight.bold), //选中文字样式
+          unselectedLabelStyle:
+              TextStyle(fontSize: 12, fontWeight: FontWeight.normal), //未选中文字样式
+          items: Constants.navigationBarItems().map(
+            (item) {
+              return _buildBottomNavItem(
+                item['title'],
+                item['normalIcon'],
+                item['pressedIcon'],
+                item['index'],
+              );
+            },
+          ).toList(),
+        ));
+  }
+
+  // 底部导航栏item
+  BottomNavigationBarItem _buildBottomNavItem(
+      String title, String normalIcon, String pressedIcon, int index) {
+    return BottomNavigationBarItem(
+      icon: Container(
+        padding: EdgeInsets.only(top: 4, bottom: 4, left: 18, right: 18),
+        decoration: BoxDecoration(
+          color: _index == index ? Colors.blueGrey.shade100 : Colors.white,
+          borderRadius: BorderRadius.circular(18),
+        ),
+        child: _index == index
+            ? Image.asset(pressedIcon, width: 24, height: 24)
+            : Image.asset(normalIcon, width: 24, height: 24),
+      ),
+      label: title,
+    );
+  }
+
   // 使用IndexedStack实现
   // @override
   // Widget build(BuildContext context) {
@@ -63,114 +131,51 @@ class HomePageState extends State<HomePage> {
   //   );
   // }
 
-  // 使用PageView实现
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      // appBar: AppBar(
-      //   title: Text('岛上码农'),
-      // ),
-      // bottomNavigationBar: _buildBottomNavigationBar(),
-      bottomNavigationBar: _buildGoogleNavBar(),
-      body: PageView(
-        physics: NeverScrollableScrollPhysics(), //禁止手势滑动
-        controller: _pageController,
-        onPageChanged: _onPageChanged,
-        children: _pageList,
-      ),
-    );
-  }
-
-  // 原生底部导航栏
-  BottomNavigationBar _buildBottomNavigationBar() {
-    return BottomNavigationBar(
-      type: BottomNavigationBarType.fixed,
-      currentIndex: _index,
-      onTap: _onBottomNagigationBarTapped,
-      selectedItemColor: Colors.blueGrey, //选中颜色
-      unselectedItemColor: Colors.grey, //未选中颜色
-      selectedLabelStyle: TextStyle(fontWeight: FontWeight.bold), //选中文字样式
-      unselectedLabelStyle: TextStyle(fontWeight: FontWeight.normal), //未选中文字样式
-      // selectedIconTheme:
-      //     IconThemeData(size: 24, color: Colors.blueAccent), //选中图标大小
-      // unselectedIconTheme: IconThemeData(size: 24), //未选中图标大小
-      items: Constants.navigationBarItems().map(
-        (item) {
-          return _buildBottomNavItem(
-            item['title'],
-            item['normalIcon'],
-            item['pressedIcon'],
-            item['index'],
-          );
-        },
-      ).toList(),
-    );
-  }
-
-  // 底部导航栏item
-  BottomNavigationBarItem _buildBottomNavItem(
-      String title, String normalIcon, String pressedIcon, int index) {
-    return BottomNavigationBarItem(
-      icon: _index == index
-          ? Image.asset(
-              pressedIcon,
-              width: 32,
-              height: 28,
-            )
-          : Image.asset(
-              normalIcon,
-              width: 32,
-              height: 28,
-            ),
-      label: title,
-    );
-  }
-
   // 谷歌底部导航栏
-  Container _buildGoogleNavBar() {
-    return Container(
-      decoration: BoxDecoration(
-        color: Colors.white,
-        boxShadow: [
-          BoxShadow(
-            blurRadius: 20,
-            color: Colors.black.withOpacity(.1),
-          )
-        ],
-      ),
-      child: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 8),
-          child: GNav(
-            rippleColor: const Color.fromARGB(255, 218, 109, 109),
-            hoverColor: const Color.fromARGB(255, 143, 84, 84),
-            tabActiveBorder: Border.all(color: Colors.grey.shade300),
-            gap: 8,
-            activeColor: Colors.black,
-            iconSize: 24,
-            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 12),
-            duration: Duration(milliseconds: 400),
-            tabBackgroundColor: Colors.grey[100]!,
-            color: Colors.black,
-            tabs: [
-              GButton(
-                icon: LineIcons.gamepad,
-                text: 'Games',
-              ),
-              GButton(
-                icon: LineIcons.history,
-                text: 'Likes',
-              ),
-              GButton(
-                icon: LineIcons.user,
-                text: 'Profile',
-              ),
-            ],
-            selectedIndex: _index,
-            onTabChange: _onBottomNagigationBarTapped,
-          ),
-        ),
-      ),
-    );
-  }
+  // Container _buildGoogleNavBar() {
+  //   return Container(
+  //     decoration: BoxDecoration(
+  //       color: Colors.white,
+  //       boxShadow: [
+  //         BoxShadow(
+  //           blurRadius: 20,
+  //           color: Colors.black.withOpacity(.1),
+  //         )
+  //       ],
+  //     ),
+  //     child: SafeArea(
+  //       child: Padding(
+  //         padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 8),
+  //         child: GNav(
+  //           rippleColor: const Color.fromARGB(255, 218, 109, 109),
+  //           hoverColor: const Color.fromARGB(255, 143, 84, 84),
+  //           tabActiveBorder: Border.all(color: Colors.grey.shade300),
+  //           gap: 8,
+  //           activeColor: Colors.black,
+  //           iconSize: 24,
+  //           padding: EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+  //           duration: Duration(milliseconds: 400),
+  //           tabBackgroundColor: Colors.grey[100]!,
+  //           color: Colors.black,
+  //           tabs: [
+  //             GButton(
+  //               icon: LineIcons.gamepad,
+  //               text: 'Games',
+  //             ),
+  //             GButton(
+  //               icon: LineIcons.history,
+  //               text: 'Likes',
+  //             ),
+  //             GButton(
+  //               icon: LineIcons.user,
+  //               text: 'Profile',
+  //             ),
+  //           ],
+  //           selectedIndex: _index,
+  //           onTabChange: _onBottomNagigationBarTapped,
+  //         ),
+  //       ),
+  //     ),
+  //   );
+  // }
 }
